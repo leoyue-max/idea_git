ff140d3712e904bdbeffbbd4744addf9b6ff46cc
leoyue <lu2011chemistry@163.com>
2020-01-02 09:38:30 +0800

    firstCommit

---
+++ src/main/java/com/leo/pojo/User.java
@@ -0,0 +1,27 @@
+package com.leo.pojo;
+
+/**
+ * @Author:Leo
+ * @Description:
+ * @Date: Created in 9:34 2020/1/2
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
